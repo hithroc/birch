53681466f440be29c63117105137ed0f0c12584e
Hithroc Mehatoko <hithroc@gmail.com>
2015-12-04 15:18:55 +0300

    Add card reading and card searching

--- src/Card/Json.hs
+++ src/Card/Json.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
 module Card.Json where
 
-import Data.Aeson.Lens (key)
+import Data.Aeson.Lens
 import Data.Aeson
+import Data.Maybe
 import Network.Wreq
 import Control.Monad.Reader
 import Control.Lens
 import Config
+import Card.Type
 import qualified Data.ByteString.Lazy as BS
 
 getLatestVersion :: (MonadReader Config m, MonadIO m) => m (Maybe String)
@@ -21,3 +23,10 @@ getVersion = do
     folder <- dataFol <$> ask
     o <- liftIO $ decode <$> (BS.readFile $ folder ++ "version.json")
     return (o ^. key "version")
+
+readCards :: (MonadReader Config m, MonadIO m) => String -> m [Card]
+readCards locale = do
+    folder <- dataFol <$> ask
+    o <- liftIO $ decode <$> (BS.readFile $ folder ++ "AllSets." ++ locale ++ ".json")
+    let c = fmap (catMaybes . map (decode . encode)) (o ^. traverseObject :: Maybe [Value])
+    return $ maybe [] id c
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Config
+import Card.Parser
+import Card.Json
+import Card.Type
+import Data.Aeson
+import Data.Aeson.Lens
+import Data.Maybe
+import Control.Lens
+import Control.Monad.Reader
+import qualified Data.ByteString.Lazy as BS
+import Data.List
+import Data.Char
+
+searchByName :: String -> [Card] -> [Card]
+searchByName n = filter (\c -> isInfixOf (map toUpper n) (map toUpper $ name c))
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    (Just cfg) <- loadConfig "config.json"
+    cards <- runReaderT (readCards "enUS") cfg
+    n <- getLine
+    putStr . unlines . map printCard . searchByName n $ cards
